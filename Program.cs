6d1548dd624906771f38d682b48d7e2bbacb990d
Darya <darya.rempel@bk.ru>
2022-11-06 16:00:28 +0600

    Программа

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿string[] array1 = new string[4] {"1", "int", "10", "print"};
+string[] array2 = new string[array1.Length];
+int x = 2;
+int y = 0;
+void CopyToSecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(count <= 1)
+        {
+        int num = new Random().Next(y,x);
+        array2[count] = array1[num];
+        count++;
+        x = 4;
+        y = 2;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+CopyToSecondArray(array1, array2);
+PrintArray(array2);
